670d13351eec34981b44144baf95fa5bda6a508a
Harry Shipton <harryjshipton@gmail.com>
2017-04-14 23:33:19 +0100

    Use newer and more secure Buffer creation methods

--- test/index.test.js
+++ test/index.test.js
@@ -6,11 +6,11 @@ chai.should();
 
 describe('Bitwise XOR', function () {
   it('should return the bitwise XOR of two Buffers', function (done) {
-    bufferEqual(xor(new Buffer('00ff', 'hex'), new Buffer('3344', 'hex')),new Buffer('33bb','hex')).should.be.true;
+    bufferEqual(xor(Buffer.from('00ff', 'hex'), Buffer.from('3344', 'hex')), Buffer.from('33bb','hex')).should.be.true;
     done();
   });
   it('should return the bitwise XOR of two Strings', function (done) {
-    bufferEqual(xor('toto', 'titi'), new Buffer('00060006','hex')).should.be.true;
+    bufferEqual(xor('toto', 'titi'), Buffer.from('00060006','hex')).should.be.true;
     done();
   });
 });
