d9b65ea6522d624d2faf7f88522dd5f20e99ad06
Harry Shipton <harryjshipton@gmail.com>
2017-04-14 23:37:25 +0100

    Use built-in buffer comparison methods

--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,15 @@
 var chai = require('chai');
-var bufferEqual = require('buffer-equal');
 var xor = require('../');
 
 chai.should();
 
 describe('Bitwise XOR', function () {
   it('should return the bitwise XOR of two Buffers', function (done) {
-    bufferEqual(xor(Buffer.from('00ff', 'hex'), Buffer.from('3344', 'hex')), Buffer.from('33bb','hex')).should.be.true;
+    xor(Buffer.from('00ff', 'hex'), Buffer.from('3344', 'hex')).equals(Buffer.from('33bb','hex')).should.be.true;
     done();
   });
   it('should return the bitwise XOR of two Strings', function (done) {
-    bufferEqual(xor('toto', 'titi'), Buffer.from('00060006','hex')).should.be.true;
+    xor('toto', 'titi').equals(Buffer.from('00060006','hex')).should.be.true;
     done();
   });
 });
