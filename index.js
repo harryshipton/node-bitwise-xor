83c5e494d4e501026c4aa419a92018fb36679cb0
Harry Shipton <harryjshipton@gmail.com>
2017-04-15 01:12:46 +0100

    Correct Buffer creation from alloc to from

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = xor;
 
 function xor(a, b) {
-  if (!Buffer.isBuffer(a)) a = Buffer.alloc(a);
-  if (!Buffer.isBuffer(b)) b = Buffer.alloc(b);
+  if (!Buffer.isBuffer(a)) a = Buffer.from(a);
+  if (!Buffer.isBuffer(b)) b = Buffer.from(b);
   var res = [];
   if (a.length > b.length) {
     for (var i = 0; i < b.length; i++) {
